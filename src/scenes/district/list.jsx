774bfddd7df55099cc0eb66b3d05687021e3b8d8
admin <dohongquantc@gmail.com>
2023-08-27 17:27:35 +0700

    27_8

--- src/scenes/district/list.jsx
+++ src/scenes/district/list.jsx
@@ -3,70 +3,82 @@ import { Link } from "react-router-dom";
 import Header from "../../components/Header";
 import { Box } from "@mui/material";
 import axiosInstance from "../../utils/axiosInstance";
-
+import ReactPaginate from "react-paginate";
 
 const DistrictList = () => {
   const [districts, setDistricts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    axiosInstance("District/paging?limit=6", "GET")
+    axiosInstance(`District/paging?limit=6&page=${currentPage + 1}`, "GET")
       .then((response) => {
         setDistricts(response.data);
-        console.log(response.data);
+        setTotalPages(10);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
-    return ( 
-        <div className="container">
-            <Box m="20px">
-        <Header
-      title="CONTACTS"
-      subtitle="List of Contacts for Future Reference"
-    />
-          <Link to={"/districtCreate"} style={{ margin: "24px 0" }}>
-            <button className="btn btn-success">
-              Create District
-            </button>
-          </Link>
+  }, [currentPage]);
 
-          <table className="table" style={{marginTop:'24px'}}>
-            <thead>
-              <th style={{width:'10%', padding:'12px'}}>STT</th>
-              <th style={{width:'25%', padding:'12px'}}>Name</th>
-              <th style={{width:'35%', padding:'12px'}}>Description</th>
-              <th style={{width:'10%', padding:'12px'}}>Status</th>
-              <th style={{width:'10%', textAlign:'center', padding:'12px'}}>Action</th>
-            </thead>
-            <tbody>
-              {districts.map((district, index) => (
-                <tr key={index}>
-                  <td style={{padding:'12px'}}>{index + 1}</td>
-                  <td style={{padding:'12px'}}>{district.name}</td>
-                  <td className="descr" style={{padding:'12px'}}>{district.description}</td>
-                  <td style={{padding:'12px'}}>{district.status ==1?"Active":"InActive"}</td>
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
 
-                  <td style={{textAlign:'right', padding:'8px'}}>
-                      <Link to={`/districtUpdate/${district.id}`} style={{marginRight:'16px'}}>
-                      <button style={{}} className="btn bg-success">
-                          <i class="fa-solid fa-pen-to-square" style={{fontSize:'20px'}}></i>
-                      </button> 
-                      </Link>
-                      <button className="btn bg-danger">
-                          <i class="fa-solid fa-trash-can" style={{fontSize:'20px'}}></i>
-                      </button>
-                  </td>
-                </tr>
-              ))}
-                
-                  
-            </tbody>
-          </table>
-
-      </Box>
+  return (
+    <div className="container">
+      <Box m="20px">
+        <Header title="CONTACTS" subtitle="List of Contacts for Future Reference" />
+        <Link to={"/districtCreate"} style={{ margin: "24px 0" }}>
+          <button className="btn btn-success">Create District</button>
+        </Link>
+        <table className="table" style={{ marginTop: "24px" }}>
+        <thead>
+            <th style={{ width: "10%", padding: "12px" }}>STT</th>
+            <th style={{ width: "25%", padding: "12px" }}>Name</th>
+            <th style={{ width: "35%", padding: "12px" }}>Description</th>
+            <th style={{ width: "10%", padding: "12px" }}>Status</th>
+            <th style={{ width: "10%", textAlign: "center", padding: "12px" }}>Action</th>
+          </thead>
+          <tbody>
+            {districts.map((district, index) => (
+              <tr key={index}>
+                <td style={{ padding: "12px" }}>{index + 1}</td>
+                <td style={{ padding: "12px" }}>{district.name}</td>
+                <td className="descr" style={{ padding: "12px" }}>
+                  {district.description}
+                </td>
+                <td style={{ padding: "12px" }}>{district.status === 1 ? "Active" : "InActive"}</td>
+                <td style={{ textAlign: "right", padding: "8px" }}>
+                  <Link to={`/districtUpdate/${district.id}`} style={{ marginRight: "16px" }}>
+                    <button style={{}} className="btn bg-success">
+                      <i class="fa-solid fa-pen-to-square" style={{ fontSize: "20px" }}></i>
+                    </button>
+                  </Link>
+                  <button className="btn bg-danger">
+                    <i class="fa-solid fa-trash-can" style={{ fontSize: "20px" }}></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "16px" }}>
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={totalPages}
+            onPageChange={handlePageChange}
+            containerClassName={"pagination"}
+            activeClassName={"active"}
+          />
+          <div style={{ marginLeft: "20px" }}>
+            Page {currentPage + 1} of {totalPages}
+          </div>
         </div>
-     );
-}
- 
+      </Box>
+    </div>
+  );
+};
+
 export default DistrictList;
